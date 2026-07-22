841b9a568850649b75793b638fc471bf29051dac
SAMUEL DOS SANTOS CARVALHO <samuel_carvalho151@estudante.sesisenai.org.br>
2026-07-22 15:33:57 -0300

    Adicionando funções de cadastro de alunos

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//////////////////////////////////PARTE 1 — Funções de apoio////////////////////////////////////////////
 let alunos = [
   { nome: "Ana", notas: [8, 7, 9] },
   { nome: "Bruno", notas: [6, 8, 7] },
@@ -10,7 +11,76 @@ for (let i = 0; i < alunos.length; i++) {
     if (alunos[i].nome.toLowerCase() === aluno.toLowerCase()) {
     return alunos[i];
 }
-return "Aluno não encontrado"; 
     }
+    return "Aluno não encontrado"; 
 }
-console.log(buscarAluno("Maranhão"))
+console.log(buscarAluno("Bruno"))
+
+//////////////////////////////PARTE 2 — Ações do Cadastro////////////////////////////////////////////////////
+
+const prompt = require("prompt-sync")();
+
+let aluno = [];
+
+function buscarAluno(nome) {
+    for (let i = 0; i < aluno.length; i++) {
+        if (aluno[i].nome === nome) {
+            return aluno[i];
+        }
+    }
+
+    return undefined;
+}
+
+function cadastrarAluno() {
+    let nome = prompt("Digite o nome do aluno:");
+
+    if (buscarAluno(nome) !== undefined) {
+        console.log("Aluno já cadastrado!");
+        return;
+    }
+
+    aluno.push({
+        nome: nome,
+        notas: []
+    });
+
+    console.log("Aluno cadastrado com sucesso!");
+}
+
+function listarAlunos() {
+    if (aluno.length === 0) {
+        console.log("Nenhum aluno cadastrado.");
+        return;
+    }
+
+    let texto = "";
+
+    for (let i = 0; i < aluno.length; i++) {
+        texto += aluno[i].nome + "\n";
+    }
+
+    console.log(texto);
+}
+
+function removerAluno() {
+    let nome = prompt("Digite o nome do aluno que deseja remover:");
+
+    let aluno = buscarAluno(nome);
+
+    if (aluno === undefined) {
+        console.log("Aluno não encontrado.");
+        return;
+    }
+
+    let posicao = alunos.indexOf(aluno);
+
+    alunos.splice(posicao, 1);
+
+    console.log("Aluno removido com sucesso!");
+}
+cadastrarAluno();
+removerAluno();
+listarAlunos();
+
+////////////////////////////////////////PARTE 3 — Ações das Notas/////////////////////////////////////////////
